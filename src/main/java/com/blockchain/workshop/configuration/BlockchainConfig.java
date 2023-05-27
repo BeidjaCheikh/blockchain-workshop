d1c745062417f78b0e87126469aecd4d336a8859
CHEIKH-BEIDJA <117984029+CHEIKH-BEIDJA@users.noreply.github.com>
2023-05-27 16:33:55 +0200

    commit

---
+++ src/main/java/com/blockchain/workshop/configuration/BlockchainConfig.java
@@ -0,0 +1,14 @@
+package com.blockchain.workshop.configuration;
+
+import com.blockchain.workshop.entity.Blockchain;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+@Configuration
+public class BlockchainConfig {
+    @Bean
+    @Scope("singleton")
+    public Blockchain blockchain(){
+        return new Blockchain(2);
+    }
+}
